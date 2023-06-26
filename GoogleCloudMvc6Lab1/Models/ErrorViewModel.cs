b1ae6f8fbcc76d71636bedb9c10d216b3148f1d3
jasper.lai <jasper.lai@gmail.com>
2023-06-26 15:26:14 +0800

    加入專案檔案。

---
+++ GoogleCloudMvc6Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoogleCloudMvc6Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
