a22759a3f9a72cb0da2d99c4d159cdcbc938d8e1
jasper.lai <jasper.lai@gmail.com>
2023-06-26 15:37:43 +0800

    get wwwroot Path in /HomeController/Index

--- GoogleCloudMvc6Lab1/Controllers/HomeController.cs
+++ GoogleCloudMvc6Lab1/Controllers/HomeController.cs
@@ -7,14 +7,18 @@
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
         }
 
         public IActionResult Index()
         {
+            ViewData["EnvironmentName"] = _env.EnvironmentName;
+            ViewData["WwwRootPath"] = _env.WebRootPath;
             return View();
         }
 
